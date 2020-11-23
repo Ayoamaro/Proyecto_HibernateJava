d5404931ac0dbc5857f988ceafa9bdcc1e967aa9
Ayoamaro <ayoze.amaro@gmail.com>
2020-11-23 22:24:32 +0000

    Upload files

---
+++ src/main/java/hibernate/Contratos.java
@@ -0,0 +1,114 @@
+package hibernate;
+
+import java.util.GregorianCalendar;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+@Entity
+@Table(name="contratos")
+public class Contratos {
+	
+	@Id
+	@Column ( columnDefinition= "int(11)")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int codContrato;
+	
+	@Column ( columnDefinition= "date")
+	private GregorianCalendar fechaInicio ;
+	
+	@Column ( columnDefinition= "date")
+	private GregorianCalendar fechaFin ; 
+			
+	@Column ( columnDefinition= "int(11)")
+	private int precioAnual ; 
+	
+	@Column ( columnDefinition= "int(11)")
+	private int precioRecision ;
+	
+	@ManyToOne (cascade={CascadeType.PERSIST}, fetch=FetchType.EAGER )
+	@JoinColumn(name="codDNIoNIE")
+	private Futbolistas futbolistaobj;
+	
+	@ManyToOne (cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name="codEquipo")
+	private Equipos equiposobj; 
+
+	public Contratos() {
+		super();
+	}
+
+	public int getCodContrato() {
+		return codContrato;
+	}
+
+	public void setCodContrato(int codContrato) {
+		this.codContrato = codContrato;
+	}
+
+	public GregorianCalendar getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(GregorianCalendar fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public GregorianCalendar getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(GregorianCalendar fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public int getPrecioAnual() {
+		return precioAnual;
+	}
+
+	public void setPrecioAnual(int precioAnual) {
+		this.precioAnual = precioAnual;
+	}
+
+	public int getPrecioRecision() {
+		return precioRecision;
+	}
+
+	public void setPrecioRecision(int precioRecision) {
+		this.precioRecision = precioRecision;
+	}
+
+	public Futbolistas getFutbolistaobj() {
+		return futbolistaobj;
+	}
+
+	public void setFutbolistaobj(Futbolistas futbolistaobj) {
+		this.futbolistaobj = futbolistaobj;
+	}
+
+	public Equipos getEquiposobj() {
+		return equiposobj;
+	}
+
+	public void setEquiposobj(Equipos equiposobj) {
+		this.equiposobj = equiposobj;
+	}
+
+	public Contratos(Futbolistas futbolistaobj, Equipos equiposobj,  GregorianCalendar fechaInicio, GregorianCalendar fechaFin, int precioAnual,
+			int precioRecision) {
+		super();
+		this.fechaInicio = fechaInicio;
+		this.fechaFin = fechaFin;
+		this.precioAnual = precioAnual;
+		this.precioRecision = precioRecision;
+		this.equiposobj = equiposobj;
+	}
+
+}
---
+++ src/main/java/hibernate/Futbolistas.java
@@ -0,0 +1,72 @@
+package hibernate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="futbolistas")
+public class Futbolistas {
+
+	@Id
+	@Column(columnDefinition= "char(9)")
+	private String codDNIoNIE;
+	
+	@Column(columnDefinition= "varchar (50)")
+	private String nombre;
+
+	@Column(columnDefinition= "varchar (40)")
+	private String nacionalidad;
+	
+	@OneToMany (cascade={CascadeType.PERSIST}, fetch=FetchType.EAGER,mappedBy="futbolistaobj" )
+	List<Contratos> contratos = new ArrayList<Contratos>();
+
+	public String getCodDNIoNIE() {
+		return codDNIoNIE;
+	}
+
+	public void setCodDNIoNIE(String codDNIoNIE) {
+		this.codDNIoNIE = codDNIoNIE;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public List<Contratos> getContratos() {
+		return contratos;
+	}
+
+	public void setContratos(List<Contratos> contratos) {
+		this.contratos = contratos;
+	}
+
+	@Override
+	public String toString() {
+		return "Futbolistas [codDNIoNIE=" + codDNIoNIE + ", nombre=" + nombre + ", nacionalidad=" + nacionalidad
+				+ ", contratos=" + contratos + "]";
+	}
+
+	public Futbolistas(String codDNIoNIE, String nombre, String nacionalidad) {
+		super();
+		this.codDNIoNIE = codDNIoNIE;
+		this.nombre = nombre;
+		this.nacionalidad = nacionalidad;	}
+
+	public Futbolistas() {
+		super();
+	} 
+}
